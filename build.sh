f384c5590c2bbbca32537889b9395fa474e51ca6
Geovanny Alzate Sandoval <vany0114@hotmail.com>
2017-12-12 21:43:33 -0500

    update dot net version

--- build.sh
+++ build.sh
@@ -8,7 +8,7 @@ dotnet restore
 # Ideally we would use the 'dotnet test' command to test netcoreapp and net451 so restrict for now 
 # but this currently doesn't work due to https://github.com/dotnet/cli/issues/3073 so restrict to netcoreapp
 
-dotnet test ./EF.DbContextFactory.UnitTest.Data -c Release -f netcoreapp1.0
+dotnet test ./EF.DbContextFactory.UnitTest.Data -c Release -f netcoreapp2.0
 
 # Instead, run directly with mono for the full .net version 
 dotnet build ./EF.DbContextFactory.UnitTest.Data -c Release -f net451
