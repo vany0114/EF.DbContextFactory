834c99ace3defc15931b84f61e714db388e7c6a8
Alexey Borodenko <kosunix@gmail.com>
2018-08-31 17:52:34 +0300

    Add SimpleInjectorExtension and ReflectionHelper class.

---
+++ EF.DbContextFactory.SimpleInjector/Extensions/SimpleInjectorExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Entity;
+using EF.DbContextFactory.SimpleInjector.Helpers;
+using SimpleInjector;
+
+namespace EF.DbContextFactory.SimpleInjector.Extensions
+{
+    public static class SimpleInjectorExtensions
+    {
+        /// <summary>
+        /// Configures the resolution of <typeparamref name="TDataContext"/>'s factory.
+        /// </summary>
+        /// <typeparam name="TDataContext">The DbContext.</typeparam>
+        /// <param name="container"></param>
+        /// <param name="nameOrConnectionString">Name or connection string of the context. (Optional)</param>
+        public static void AddDbContextFactory<TDataContext>(this Container container, string nameOrConnectionString = null)
+            where TDataContext : DbContext
+        {
+            nameOrConnectionString = nameOrConnectionString ?? "DefaultConnection";
+            container.Register<Func<TDataContext>>(() =>
+            {
+                if (!string.IsNullOrEmpty(nameOrConnectionString) && ReflectionHelper.HasContructorWithConnectionString<TDataContext>())
+                    return () => (TDataContext)Activator.CreateInstance(typeof(TDataContext), nameOrConnectionString);
+
+                return () => (TDataContext)Activator.CreateInstance(typeof(TDataContext));
+            });
+        }
+    }
+}
