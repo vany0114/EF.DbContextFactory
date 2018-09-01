05e2d533f22d60c2bcce188bc33e57e7e87e5982
Alexey Borodenko <kosunix@gmail.com>
2018-09-01 16:03:09 +0300

    Add project with integration SimpleInjector and MVC.

---
+++ EF.DbContextFactory.Examples.SimpleInjector/App_Start/SimpleInjectorMvc.cs
@@ -0,0 +1,32 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+using EF.DbContextFactory.Examples.Data.Persistence;
+using EF.DbContextFactory.Examples.Data.Repository;
+using EF.DbContextFactory.SimpleInjector.Extensions;
+using SimpleInjector;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
+
+namespace EF.DbContextFactory.Examples.SimpleInjector
+{
+    public static class SimpleInjectorMvc
+    {
+        public static void Register()
+        {
+            var container = new Container();
+            container.Options.DefaultScopedLifestyle = new WebRequestLifestyle();
+
+            container.Register<OrderContext>(Lifestyle.Scoped);
+            container.AddDbContextFactory<OrderContext>();
+
+            container.Register<OrderRepository>();
+            container.Register<OrderRepositoryWithFactory>();
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+
+            container.Verify();
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+    }
+}
